227dad5ca094a4aa69ae70f836de2403dac8a269
Tuan Pham <tuanvpham32@outlook.com>
2018-10-31 17:10:43 -0400

    update settings for database

--- agilecommandcentral/settings.py
+++ agilecommandcentral/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'agilecommandcentral.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'agilecommandcentral',
+        'USER': 'group10',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
