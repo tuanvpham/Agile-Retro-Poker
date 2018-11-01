d4d85d0313302aa52940f5e7403e33fc4e74e058
Kyle Capehart <doctorelyk@gmail.com>
2018-11-01 00:18:38 -0400

    created db and inserted some data

--- core/models.py
+++ core/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 
 class Session(models.Model):
     name = models.CharField(max_length=30)
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class User(AbstractUser):
+    role_id = models.ForeignKey(Role, on_delete=models.CASCADE)
+    session_id = models.ForeignKey(Session, on_delete=models.CASCADE)
+
+
+class Story(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    story_points = models.IntegerField()
+    session_id = models.ForeignKey(Session, on_delete=models.CASCADE)
--- core/views.py
+++ core/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
-
+from .models import Session, Role, User, Story
 
 def index(request):
-    return HttpResponse("Hello, this is agile central command")
+    s = Session(name="Planning Poker")
+    r = Role(name="Admin")
+    u = User(username="k-dog", password="password", session_id=s, role_id=r)
+    t = Story(name="User Story - 1", story_points=10, session_id=s)
+
+    message = u.username + "(" + r.name + ")" + " is in a session of " + s.name + ", and is looking at " + t.name
+
+    return HttpResponse(message)
+    
+
+    #return HttpResponse("Hello, this is agile central command")
+
