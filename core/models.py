2eb1c5f96f0cf8fee51365e7a9b854cd89f0e5e4
Tuan Pham <tuanvpham32@outlook.com>
2018-11-04 15:48:09 -0500

    add str for models

--- core/models.py
+++ core/models.py
@@ -1,23 +1,38 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 
 
 class Session(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 
 class Role(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 
 class User(AbstractUser):
-    role_id = models.ForeignKey(Role, on_delete=models.CASCADE)
-    session_id = models.ForeignKey(Session, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s with %s role and %s session' % (
+            self.username,
+            self.role,
+            self.session
+        )
 
 
 class Story(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
     story_points = models.IntegerField()
-    session_id = models.ForeignKey(Session, on_delete=models.CASCADE)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
