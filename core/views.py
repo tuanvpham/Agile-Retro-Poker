026fd873941bd4c2794210fe50458b11dce02a08
LeeWildes <leeawildes@knights.ucf.edu>
2018-11-29 16:45:17 -0500

    testing some views stuff again

--- core/views.py
+++ core/views.py
@@ -3,18 +3,19 @@ from .models import Session, Role, User, Story
 
 
 def index(request):
-    """s = Session(name="Planning Poker")
+    s = Session(name="Planning Poker")
     r = Role(name="Admin")
     u = User(username="k-dog", password="password", session_id=s, role_id=r)
     t = Story(name="User Story - 1", story_points=10, session_id=s)
 
     s.save()
     r.save()
-    u.save()
+    ## u.save()
     t.save()
 
-    message = u.username + "(" + r.name + ")" + " is in a session of " + s.name + ", and is looking at " + t.name
+    ## message = u.username + "(" + r.name + ")" + " is in a session of " + s.name + ", and is looking at " + t.name
+    message = "(" + r.name + ")" + " is in a session of " + s.name + ", and is looking at " + t.name
 
-    return HttpResponse(message)"""
+    return HttpResponse(message)
 
-    return HttpResponse("HelloWorld")
+    ## return HttpResponse("HelloWorld")
