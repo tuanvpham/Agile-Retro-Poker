5663f250f4545bc4c15a3d4731cc8a1148052714
LeeWildes <leeawildes@knights.ucf.edu>
2018-11-29 16:22:31 -0500

    testing some ish

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from .models import Session, Role, User, Story
 
 
 def index(request):
-    s = Session(name="Planning Poker")
+    """s = Session(name="Planning Poker")
     r = Role(name="Admin")
     u = User(username="k-dog", password="password", session_id=s, role_id=r)
     t = Story(name="User Story - 1", story_points=10, session_id=s)
@@ -15,4 +15,6 @@ def index(request):
 
     message = u.username + "(" + r.name + ")" + " is in a session of " + s.name + ", and is looking at " + t.name
 
-    return HttpResponse(message)
+    return HttpResponse(message)"""
+
+    return HttpResponse("HelloWorld")
