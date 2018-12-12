7c426fbf85534317a7dfb46573e9a5e9b9512891
Tuan Pham <tuanvpham32@outlook.com>
2018-12-12 18:14:21 -0500

    update views with simple response

--- core/views.py
+++ core/views.py
@@ -3,16 +3,16 @@ from .models import Session, Role, User, Story
 
 
 def index(request):
-    s = Session(name="Planning Poker")
-    r = Role(name="Admin")
-    u = User(username="k-dog", password="password", session_id=s, role_id=r)
-    t = Story(name="User Story - 1", story_points=10, session_id=s)
+    # s = Session(name="Planning Poker")
+    # r = Role(name="Admin")
+    # u = User(username="k-dog", password="password", session_id=s, role_id=r)
+    # t = Story(name="User Story - 1", story_points=10, session_id=s)
 
-    s.save()
-    r.save()
-    u.save()
-    t.save()
+    # s.save()
+    # r.save()
+    # u.save()
+    # t.save()
 
-    message = u.username + "(" + r.name + ")" + " is in a session of " + s.name + ", and is looking at " + t.name
+    # message = u.username + "(" + r.name + ")" + " is in a session of " + s.name + ", and is looking at " + t.name
 
-    return HttpResponse(message)
+    return HttpResponse("this is agile command central")
