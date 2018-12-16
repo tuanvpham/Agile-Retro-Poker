234d3adf672bdb2c7521d7e71f11135e3870c98a
Tuan Pham <tuanvpham32@outlook.com>
2018-12-16 01:19:29 -0500

    update admin and urls

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf.urls import url, include
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.login_view, name='login_view'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
