fa3e3c3235f5d44b6e8f2497c88718e0c53d2c3e
Tuan Pham <tuanvpham32@outlook.com>
2018-12-19 01:20:16 -0500

    update login view

--- core/views.py
+++ core/views.py
@@ -1,18 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required, user_passes_test
-from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 
 from jira import JIRA, JIRAError
+from rest_framework import viewsets, authentication, permissions
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 from .models import Session, Role, Story, User
+from .serializers import *
 
 
+# Rest API
+@api_view(['POST'])
 def login_view(request):
+    """
+    Login API: input login data from clients
+    """
     if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
+        email = request.data['user']['email']
+        password = request.data['user']['password']
         try:
             jac = JIRA(
                 'https://agilecommand.atlassian.net',
@@ -21,17 +31,31 @@ def login_view(request):
             jac_username = jac.myself().get('displayName')
             user = authenticate(request, username=email)
             if user is None:
-                user = User(email=email, username=jac_username)
+                user = User(
+                    email=email, username=jac_username, password=password
+                )
                 user.save()
             login(request, user)
-            return redirect('dashboard')
+            return Response(status=status.HTTP_201_CREATED)
         except JIRAError as e:
             print('This is jira error: ' + str(e.status_code))
-            return HttpResponse('Wrong login info')
-
-    return render(request, 'login.html')
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-@login_required(login_url='/')
-def dashboard(request):
-    return HttpResponse('dashboard')
+# @login_required(login_url='/')
+# def dashboard(request):
+#     return render(request, 'dashboard.html')
+
+
+# @login_required(login_url='/')
+# def logout_view(request):
+#     logout(request)
+#     return redirect('core:login_view')
+
+
+# Rest API for clients
+class UserView(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    http_method_names = ['post', 'head', 'options']
