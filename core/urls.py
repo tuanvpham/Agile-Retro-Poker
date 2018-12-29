ea652e0c4c8d67ab25db1a28221ea657f6f8f640
Tuan Pham <tuanvpham32@outlook.com>
2018-12-28 22:31:49 -0500

    update api view for authentication

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,13 @@
 from django.urls import path
 from django.conf.urls import url, include
 from rest_framework import routers, serializers, viewsets
+from rest_framework_jwt.views import obtain_jwt_token
+
 
 from . import views
-app_name = 'core'
-# router = routers.DefaultRouter()
-# router.register('users', views.login_view)
 
 urlpatterns = [
-    # path('', views.login_view, name='login_view'),
-    # path('dashboard/', views.dashboard, name='dashboard'),
-    # path('logout/', views.logout_view, name='logout_view'),
-    # Restful API
-    # path('api/', include(router.urls)),
-    url(r'^api/users/login', views.login_view),
-    # path('api/login', views.login_view, name='login_view'),
+    path('token-auth/', obtain_jwt_token),
+    path('current_user/', views.current_user),
+    path('users/', views.UserList.as_view())
 ]
--- core/views.py
+++ core/views.py
@@ -1,61 +1,108 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.decorators import login_required
 
 from jira import JIRA, JIRAError
-from rest_framework import viewsets, authentication, permissions
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.views import APIView
+from rest_framework_jwt.settings import api_settings
 
-from .models import Session, Role, Story, User
+
+from .models import *
 from .serializers import *
 
 
-# Rest API
-@api_view(['POST'])
-def login_view(request):
-    """
-    Login API: input login data from clients
-    """
-    if request.method == 'POST':
-        email = request.data['user']['email']
-        password = request.data['user']['password']
+# Rest API View
+@api_view(['GET'])
+def current_user(request):
+    '''
+    Determine the current user by their token, and return their data
+    '''
+
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
+
+
+class UserList(APIView):
+    '''
+    Login with Jira Auth
+    If user exists in database, return user data and new token
+    Else create new user and return user data
+    '''
+
+    permission_classes = (AllowAny,)
+
+    def get_object(self, email):
         try:
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            return None
+
+    def post(self, request, format=None):
+        email = request.data['email']
+        password = request.data['password']
+        try:
+            # Login with Jira Auth
             jac = JIRA(
-                'https://agilecommand.atlassian.net',
+                'https://agilecommandcentral.atlassian.net',
                 basic_auth=(email, password)
             )
             jac_username = jac.myself().get('displayName')
-            user = authenticate(request, username=email)
+            user = self.get_object(email)
             if user is None:
-                user = User(
-                    email=email, username=jac_username, password=password
+                # Signup with ACC
+                serializer = UserSerializerWithToken(data={
+                    'email': email,
+                    'username': jac_username,
+                    'password': password
+                })
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(
+                        serializer.data,
+                        status=status.HTTP_201_CREATED
+                    )
+                return Response(
+                    serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST
                 )
-                user.save()
-            login(request, user)
-            return Response(status=status.HTTP_201_CREATED)
+            else:
+                token = generate_new_token(user)
+                user_existed_data = {
+                    'token': token,
+                    'email': user.email,
+                    'username': user.username
+                }
+                return Response(
+                    user_existed_data,
+                    status=status.HTTP_200_OK
+                )
         except JIRAError as e:
-            print('This is jira error: ' + str(e.status_code))
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response(status=status.HTTP_408_REQUEST_TIMEOUT)
 
 
-# @login_required(login_url='/')
-# def dashboard(request):
-#     return render(request, 'dashboard.html')
+# Utitlities
+def my_jwt_response_handler(token, user=None, request=None):
+    '''
+        Return response includes token, email, username
+    '''
+    return {
+        'token': token,
+        'user': UserSerializer(user, context={'request': request}).data
+    }
 
 
-# @login_required(login_url='/')
-# def logout_view(request):
-#     logout(request)
-#     return redirect('core:login_view')
+def generate_new_token(user):
+    '''
+        Return token for authenticated user
+    '''
+    jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
-
-# Rest API for clients
-class UserView(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    http_method_names = ['post', 'head', 'options']
+    payload = jwt_payload_handler(user)
+    token = jwt_encode_handler(payload)
+    return token
