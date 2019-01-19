acf712835b716314f6d69e648a7c7994f87187de
Tuan Pham <tuanvpham32@outlook.com>
2019-01-19 01:02:20 -0500

    update retro ws

--- core/routing.py
+++ core/routing.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r'^ws/retro/(?P<session_name>[^/]+)/$', consumers.SessionConsumer),
+    url(r'^retro/(?P<session_name>[^/]+)/$', consumers.SessionConsumer),
 ]
