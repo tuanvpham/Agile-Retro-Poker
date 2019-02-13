205e48d9b1c206575ead617fcb145f9efe15fbf1
Tuan Pham <tuanvpham32@outlook.com>
2019-02-12 22:54:32 -0500

    update consumer for ending and exiting session

--- core/consumers.py
+++ core/consumers.py
@@ -30,11 +30,12 @@ class SessionConsumer(WebsocketConsumer):
 
             self.accept()
         else:
-            print('DISCONNECT')
+            print('DISCONNECT - MISSING SESSION')
             self.close()
 
     def disconnect(self, close_code):
         # Leave session
+        print('DISCONNECT - WEBSOCKET CLOSED')
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
@@ -43,50 +44,89 @@ class SessionConsumer(WebsocketConsumer):
     # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        print(text_data_json)
-        item_type = text_data_json['itemType']
-        item_text = text_data_json['itemText']
-
-        session = get_session_object(
-            self.scope['url_route']['kwargs']['session_name']
-        )
-
-        if item_type == 'what_went_well':
-            retro_board_item = RetroBoardItems.objects.create(
-                owner=self.scope['user'],
-                session=session,
-                item_type='WWW',
-                item_text=item_text
+        if 'end_session' in text_data_json:
+            session = get_session_object(
+                self.scope['url_route']['kwargs']['session_name']
             )
-            retro_board_item.save()
-        elif item_type == 'what_did_not':
-            retro_board_item = RetroBoardItems.objects.create(
-                owner=self.scope['user'],
-                session=session,
-                item_type='WDN',
-                item_text=item_text
+            owner = User.objects.get(id=session.owner_id)
+            session = get_session_object(
+                self.scope['url_route']['kwargs']['session_name']
             )
-            retro_board_item.save()
+
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'end_session_by_owner',
+                    'session_owner': owner.username,
+                    'message': text_data_json['end_session']
+                }
+            )
+
+            Session.objects.get(id=session.id).delete()
+        elif 'exit_session' in text_data_json:
+            member = User.objects.get(username=text_data_json['session_member'])
+            session = get_session_object(
+                self.scope['url_route']['kwargs']['session_name']
+            )
+            session_member = SessionMember.objects.get(
+                session=session.id,
+                member=member.id
+            )
+
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'exit_session_by_user',
+                    'member': member.username,
+                    'message': text_data_json['exit_session']
+                }
+            )
+
+            session_member.delete()
         else:
-            retro_board_item = RetroBoardItems.objects.create(
-                owner=self.scope['user'],
-                session=session,
-                item_type='AI',
-                item_text=item_text
-            )
-            retro_board_item.save()
+            item_type = text_data_json['itemType']
+            item_text = text_data_json['itemText']
 
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'send_out_retro_board_item_to_websocket',
-                'item_id': retro_board_item.id,
-                'item_owner': self.scope['user'].username,
-                'item_type': retro_board_item.item_type,
-                'item_text': item_text
-            }
-        )
+            session = get_session_object(
+                self.scope['url_route']['kwargs']['session_name']
+            )
+
+            if item_type == 'what_went_well':
+                retro_board_item = RetroBoardItems.objects.create(
+                    owner=self.scope['user'],
+                    session=session,
+                    item_type='WWW',
+                    item_text=item_text
+                )
+                retro_board_item.save()
+            elif item_type == 'what_did_not':
+                retro_board_item = RetroBoardItems.objects.create(
+                    owner=self.scope['user'],
+                    session=session,
+                    item_type='WDN',
+                    item_text=item_text
+                )
+                retro_board_item.save()
+            else:
+                retro_board_item = RetroBoardItems.objects.create(
+                    owner=self.scope['user'],
+                    session=session,
+                    item_type='AI',
+                    item_text=item_text
+                )
+                retro_board_item.save()
+
+            # Send message to room group
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'send_out_retro_board_item_to_websocket',
+                    'item_id': retro_board_item.id,
+                    'item_owner': self.scope['user'].username,
+                    'item_type': retro_board_item.item_type,
+                    'item_text': item_text
+                }
+            )
 
     def send_out_retro_board_item_to_websocket(self, event):
         item_id = event['item_id']
@@ -99,4 +139,20 @@ class SessionConsumer(WebsocketConsumer):
             'item_owner': item_owner,
             'item_type': item_type,
             'item_text':  item_text
+        }))
+
+    def end_session_by_owner(self, event):
+        end_session_message = event['message']
+        session_owner = event['session_owner']
+        self.send(text_data=json.dumps({
+            'session_owner': session_owner,
+            'end_session_message': end_session_message
+        }))
+
+    def exit_session_by_user(self, event):
+        exit_session_message = event['message']
+        member = event['member']
+        self.send(text_data=json.dumps({
+            'member': member,
+            'exit_session_message': exit_session_message
         }))
