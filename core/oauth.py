add7719fca9db22d3e300fa403adcf334cc48d25
Tuan Pham <tuanvpham32@outlook.com>
2019-03-23 11:08:37 -0400

    revert again

---
+++ core/oauth.py
@@ -0,0 +1,68 @@
+import requests
+from oauthlib.oauth1 import SIGNATURE_RSA
+from requests_oauthlib import OAuth1Session
+from jira.client import JIRA
+
+
+def read(file_path):
+    """ Read a file and return it's contents. """
+    with open(file_path) as f:
+        return f.read()
+
+
+# The Consumer Key created while setting up the "Incoming Authentication" in
+# JIRA for the Application Link.
+CONSUMER_KEY = 'OauthKey'
+CONSUMER_SECRET = 'dont_care'
+VERIFIER = 'jira_verifier'
+
+# The contents of the rsa.pem file generated (the private RSA key)
+RSA_KEY = read('jira_privatekey.pem')
+
+# The URLs for the JIRA instance
+JIRA_SERVER = 'https://agilecommandcentralgroup10.atlassian.net'
+REQUEST_TOKEN_URL = JIRA_SERVER + '/plugins/servlet/oauth/request-token'
+AUTHORIZE_URL = JIRA_SERVER + '/plugins/servlet/oauth/authorize'
+ACCESS_TOKEN_URL = JIRA_SERVER + '/plugins/servlet/oauth/access-token'
+
+
+def connect_1():
+    # Step 1: Get a request token
+    oauth = OAuth1Session(CONSUMER_KEY, signature_type='auth_header',
+                          signature_method=SIGNATURE_RSA, rsa_key=RSA_KEY)
+    request_token = oauth.fetch_request_token(REQUEST_TOKEN_URL)
+    resource_owner_key = request_token['oauth_token']
+    resource_owner_secret = request_token['oauth_token_secret']
+
+    redir_url = (AUTHORIZE_URL + "?oauth_token=" +
+                request_token['oauth_token'] +
+                "&oauth_callback=http://localhost:3000/oauth_user" +
+                "?oauth_token_secret=" + resource_owner_secret)
+
+    access_tokens = {
+        'oauth_token': resource_owner_key,
+        'oauth_token_secret': resource_owner_secret,
+        'oauth_url': redir_url
+    }
+    return access_tokens
+
+
+def connect_2(resource_owner_key, resource_owner_secret):
+    oauth = OAuth1Session(
+        CONSUMER_KEY,
+        client_secret=CONSUMER_SECRET,
+        resource_owner_key=resource_owner_key,
+        resource_owner_secret=resource_owner_secret,
+        verifier=VERIFIER,
+        signature_method=SIGNATURE_RSA,
+        rsa_key=RSA_KEY
+    )
+    # Step 3: Get the access token
+    access_token = oauth.fetch_access_token(ACCESS_TOKEN_URL)
+    jira_options = {
+        'access_token': access_token['oauth_token'],
+        'access_token_secret': access_token['oauth_token_secret'],
+        'consumer_key': CONSUMER_KEY,
+        'key_cert': RSA_KEY
+    }
+    return jira_options
--- core/views.py
+++ core/views.py
@@ -1,21 +1,120 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404, redirect
-
-
 from jira import JIRA, JIRAError
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet
-from rest_framework_jwt.settings import api_settings
 
 from .models import *
 from .serializers import *
+from .utilities import *
+from .oauth import *
 
 
-# Rest API View
+JIRA_SERVER = 'https://agilecommandcentralgroup10.atlassian.net'
+CONSUMER_KEY = 'OauthKey'
+CONSUMER_SECRET = 'dont_care'
+VERIFIER = 'jira_verifier'
+RSA_KEY = read('jira_privatekey.pem')
+
+
+class Login(APIView):
+    '''
+    Handle Login Submission
+    '''
+
+    permission_classes = (AllowAny,)
+
+    def get(self, request, format=None):
+        response_data = ({
+            'message': "Successful"
+        })
+        status_code = status.HTTP_400_BAD_REQUEST
+        try:
+            access_tokens = connect_1()
+            response_data = {**response_data, **access_tokens}
+            status_code = status.HTTP_200_OK
+        except JIRAError as e:
+            response_data = ({
+                'message': 'Cannot connect to JIRA Server. Please try again later.'
+            })
+        finally:
+            return Response(response_data, status=status_code)
+
+
+@api_view(['POST'])
+@permission_classes((AllowAny, ))
+def oauth_user(request):
+    response_data = {
+        'message': "Success"
+    }
+    status_code = status.HTTP_400_BAD_REQUEST
+    try:
+        try:
+            oauth_token = request.data['oauth_token']
+            oauth_token_secret = request.data['oauth_token_secret']
+        except:
+            oauth_token = request.query_params['oauth_token']
+            oauth_token_secret = request.query_params['oauth_token_secret']
+
+        jira_options = connect_2(oauth_token, oauth_token_secret)
+        jac = JIRA(
+            options={'server': JIRA_SERVER},
+            oauth=jira_options
+        )
+        jac_username = jac.myself().get('displayName')
+        jac_email = jac.myself().get('emailAddress')
+        access_tokens = {
+            'access_token': jira_options['access_token'],
+            'secret_access_token': jira_options['access_token_secret']
+        }
+
+        user = get_user_object(jac_email)
+        if user is None:
+            # Signup with ACC
+            # convert the user data to a byte stream
+            serializer = UserSerializerWithToken(data={
+                'email': jac_email,
+                'username': jac_username,
+            })
+            # if it is possible to deserialize the data then success
+            if serializer.is_valid():
+                serializer.save()
+                response_data = ({
+                    'message': "Successfully logged in user for the first time"
+                })
+                response_data = {**response_data, **
+                                 serializer.data, **access_tokens}
+                status_code = status.HTTP_201_CREATED
+            # otherwise get the errors
+            else:
+                response_data = ({
+                    'message': "There was a problem creating the details for this user"
+                })
+                response_data = {**response_data, **serializer.errors}
+                status_code = status.HTTP_400_BAD_REQUEST
+        else:
+            # generate a token for the session if the credentials were valid
+            token = generate_new_token(user)
+            user_data = ({
+                'token': token,
+                'email': user.email,
+                'username': user.username
+            })
+            response_data = ({
+                'message': "Successfully logged in",
+            })
+            response_data = {**response_data, **user_data, **access_tokens}
+            status_code = status.HTTP_200_OK
+        return Response(data=response_data, status=status.HTTP_200_OK)
+    except:
+        response_data = {
+            'message': "Error"
+        }
+        return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
+
 @api_view(['GET'])
 def current_user(request):
     '''
@@ -26,97 +125,279 @@ def current_user(request):
     return Response(serializer.data)
 
 
-class UserAuthentication(APIView):
+@api_view(['POST'])
+def delete_session(request):
+    try:
+        session = Session.objects.get(id=request.data['session'])
+        session.delete()
+        return Response(status=status.HTTP_200_OK)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class SessionCreate(APIView):
     '''
-    Handle authentication with tokens
+    Fetch and create sessions
     '''
 
-    permission_classes = (AllowAny,)
-
-    def get_object(self, email):
-        try:
-            return User.objects.get(email=email)
-        except User.DoesNotExist:
-            return None
+    def get(self, request, format=None):
+        sessions = Session.objects.all()
+        session_serializer = SessionSerializer(sessions, many=True)
+        return Response(session_serializer.data)
 
     def post(self, request, format=None):
-        email = request.data['email']
-        password = request.data['password']
         try:
-            # Login with Jira Auth
-            jac = JIRA(
-                'https://agilecommandcentralgroup10.atlassian.net/',
-                basic_auth=(email, password)
-            )
-            jac_username = jac.myself().get('displayName')
-            user = self.get_object(email)
-            if user is None:
-                # Signup with ACC
-                serializer = UserSerializerWithToken(data={
-                    'email': email,
-                    'username': jac_username,
-                    'password': password
-                })
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(
-                        serializer.data,
-                        status=status.HTTP_201_CREATED
-                    )
-                return Response(
-                    serializer.errors,
-                    status=status.HTTP_400_BAD_REQUEST
+            owner = get_user_object(request.data['email'])
+            session = get_session_object(request.data['title'])
+            if session is None:
+                session_type = 'R'
+                if request.data['session_type'] == 'poker':
+                    session_type = 'P'
+                session = Session(
+                    title=request.data['title'],
+                    session_type=session_type,
+                    owner=owner
                 )
-            else:
-                token = generate_new_token(user)
-                user_existed_data = {
-                    'token': token,
-                    'email': user.email,
-                    'username': user.username
-                }
+                session.save()
+                response_data = ({
+                    'id': session.id,
+                    'title': session.title,
+                    'session_type': session.session_type,
+                    'owner': owner.username
+                })
                 return Response(
-                    user_existed_data,
+                    data=response_data,
                     status=status.HTTP_200_OK
                 )
-        except JIRAError as e:
-            return Response(status=status.HTTP_408_REQUEST_TIMEOUT)
+            else:
+                return Response(
+                    data={'error_message': "Duplicate title. Please choose another name for title"},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-class SessionViewSet(viewsets.ModelViewSet):
+class RetroBoardItemsList(generics.ListAPIView):
     '''
-    Set of Session views for listing, retrieving, creating, deleting sessions
-    '''
-
-    queryset = Session.objects.all()
-    serializer_class = SessionSerializer
-
-
-# Test deploy
-@api_view(['GET'])
-@permission_classes((AllowAny, ))
-def test_deploy(request):
-    return HttpResponse(content='You made it')
-
-
-# Utitlities
-def my_jwt_response_handler(token, user=None, request=None):
-    '''
-        Return response includes token, email, username
+    Returns all retro board items
     '''
 
-    return {
-        'token': token,
-        'user': UserSerializer(user, context={'request': request}).data
-    }
+    queryset = RetroBoardItems.objects.all()
+    serializer_class = RetroBoardItemsSerializer
 
 
-def generate_new_token(user):
+@api_view(['POST'])
+def remove_stories(request):
+    try:
+        stories = request.data['stories']
+        for story in stories:
+            if(story['selected'] is False):
+                Story.objects.get(id=story['id']).delete()
+        return Response(status=status.HTTP_200_OK)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class StoryItemList(generics.ListAPIView):
     '''
-        Return token for authenticated user
+    Returns all story items
     '''
-    jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
-    payload = jwt_payload_handler(user)
-    token = jwt_encode_handler(payload)
-    return token
+    queryset = Story.objects.all()
+    serializer_class = StorySerializer
+
+
+class StorySelectList(APIView):
+    '''
+    Returns all stories from Jira backlog for Planning Poker
+    '''
+
+    def post(self, request, format=None):
+        try:
+            jira_options = {
+                'access_token': request.data['access_token'],
+                'access_token_secret': request.data['secret_access_token'],
+                'consumer_key': CONSUMER_KEY,
+                'key_cert': RSA_KEY
+            }
+            jac = JIRA(
+                options={'server': JIRA_SERVER},
+                oauth=jira_options
+            )
+            stories = jac.search_issues('issueType=Story')
+            session_id = Session.objects.get(id=request.data['session'])
+            for story in stories:
+                story = Story(
+                    title=story.fields.summary,
+                    description=story.fields.description,
+                    story_points=story.fields.customfield_10024,
+                    session=session_id,
+                    key=story.key
+                )
+                story.save()
+            return Response(data={'message': "Successfully retrieved from Jira"}, status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def check_session_owner(request):
+    current_session = Session.objects.get(title=request.data['session_title'])
+    if request.user == current_session.owner:
+        data = {
+            'is_owner': True
+        }
+    else:
+        data = {
+            'is_owner': False
+        }
+
+    return Response(data)
+
+
+class SessionMemberList(APIView):
+    '''
+        Retrieve all members of a specific session
+    '''
+
+    def get(self, request, session_id, format=None):
+        member_list = SessionMember.objects.filter(session_id=session_id)
+        serializer = SessionMemberSerializer(member_list, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['POST'])
+def update_points(request):
+    try:
+        story = Story.objects.get(key=request.data['key'])
+        story.story_points = request.data['points']
+        story.save(update_fields=["story_points"])
+        return Response(status=status.HTTP_200_OK)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def end_retro(request):
+    '''
+    When a retro session ends, post all action items to Jira
+    '''
+
+    try:
+        items = list(
+            RetroBoardItems.objects.filter(
+                item_type='AI', session=request.data['session']
+            )
+        )
+        jira_options = {
+            'access_token': request.data['access_token'],
+            'access_token_secret': request.data['secret_access_token'],
+            'consumer_key': CONSUMER_KEY,
+            'key_cert': RSA_KEY
+        }
+        jac = JIRA(
+            options={'server': JIRA_SERVER},
+            oauth=jira_options
+        )
+        session = Session.objects.get(id=request.data['session'])
+        for i in items:
+            jac.create_issue(
+                project='AG',
+                summary=i.item_text,
+                issuetype={'name': 'Task'},
+                description="Access Item from Retro Session: " + session.title
+            )
+        session.delete()
+        return Response(status=status.HTTP_200_OK)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class Stories(APIView):
+    '''
+        Retrieve a specific story
+    '''
+
+    def get(self, request, session_id, format=None):
+        story_list = Story.objects.filter(session_id=session_id)
+        serializer = StorySerializer(story_list, many=True)
+        return Response(serializer.data)
+
+
+class Cards(APIView):
+    '''
+        Retrive cards for a specif story and session
+    '''
+
+    def get(self, request, session_id, story_id, format=None):
+        card_list = Card.objects.filter(
+            session_id=session_id,
+            story_id=story_id
+        )
+        serializer = CardSerializer(card_list, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['POST'])
+def update_points(request):
+    try:
+        story = Story.objects.get(key=request.data['key'])
+        story.story_points = request.data['points']
+        story.save(update_fields=["story_points"])
+        return Response(status=status.HTTP_200_OK)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def end_poker(request):
+    '''
+    Update all stories in the database in Jira when Planning Poker session ends
+    '''
+
+    try:
+        jira_options = {
+            'access_token': request.data['access_token'],
+            'access_token_secret': request.data['secret_access_token'],
+            'consumer_key': CONSUMER_KEY,
+            'key_cert': RSA_KEY
+        }
+        jac = JIRA(
+            options={'server': JIRA_SERVER},
+            oauth=jira_options
+        )
+        current_session = Session.objects.get(id=request.data['session'])
+        stories = Story.objects.filter(session=current_session)
+        for story in stories:
+            jac.issue(story.key).update(customfield_10024=story.story_points)
+        current_session.delete()
+        return Response(status=status.HTTP_200_OK)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def check_session_started(request):
+    try:
+        current_session = Session.objects.get(
+            title=request.data['session_title']
+        )
+    except Session.DoesNotExist:
+        current_session = None
+
+    if current_session is None:
+        data = {
+            'error': 'Session does not exits'
+        }
+    else:
+        if current_session.is_started is True:
+            data = {
+                'is_started': True
+            }
+        else:
+            data = {
+                'is_started': False
+            }
+
+    return Response(data)
